ac9f4aec918b7a58d373ee0418c0cb598dd74c62
Jim Garvin <jim@thegarvin.com>
2022-09-07 13:21:05 -0400

    Bumps version to 1.1.0

--- lib/working_set.rb
+++ lib/working_set.rb
@@ -3,7 +3,7 @@ require 'set'
 class WorkingSet
   attr_accessor :search, :options, :items, :name, :note, :saved
 
-  VERSION = "1.0.2"
+  VERSION = "1.1.0"
 
   def initialize(search = nil, options = nil, items = [])
     self.search  = search
